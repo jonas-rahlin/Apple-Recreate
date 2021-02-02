146ee3c95849df88ec610be83faf9bd2feaad83c
Leafhill <69792376+jonas-rahlin@users.noreply.github.com>
2021-02-02 14:50:59 +0100

    New commit
    Added lots of styling and functional updates to the navigation bar search function.
    
    Styling.
    Animations.
    Clickevents.

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const searchButton = document.querySelector("#nav-links-h");
 const searchLinks = document.querySelector("#nav-search");
 const disablingLayer = document.querySelector("#disabling-layer");
-const navLinks = document.querySelector("nav");
+const body = document.querySelector("body");
+const nav = document.querySelector("nav");
+const navLinks = document.querySelector("#nav-links");
+const searchClose = document.querySelector("#nav-search-bar-icon2");
+const clickElements = [disablingLayer, searchClose, searchButton];
 
-
-searchButton.addEventListener("click", function(e) {
+//function for toggling various css classes(animations) on the navbar, when activating the search input.
+const toggles = () => {
     searchLinks.classList.toggle("hidden");
     disablingLayer.classList.toggle("hidden");
-});
+    nav.classList.toggle("navcolor");
+    navLinks.classList.toggle("hidden2");
+    body.classList.toggle("scroll-lock");
+    if(searchLinks.classList.contains("hidden")){
+        navLinks.classList.add("fade-in");
+    }
+    else{
+        navLinks.classList.remove("fade-in");
+    }
+}
 
-disablingLayer.addEventListener("click", function() {
-    searchLinks.classList.toggle("hidden");
-    disablingLayer.classList.toggle("hidden");
+// loop for adding clickevents (wich triggers the toggles function) to multiple elements.
+clickElements.forEach((e) => {
+    e.addEventListener("click", () => {
+        toggles()
+    });
 });
